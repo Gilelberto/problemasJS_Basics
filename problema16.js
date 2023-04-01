9eff9ce3605d3abcb4d78f9656a9d4c921d36551
Gil C <a348503@uach.mx>
2023-04-01 12:28:26 -0600

    Se agrega el archivo problema16.js

---
+++ problema16.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 10; i++){
+    console.log(`=====Iteración número: ${i+1}=====`);
+    let numbers = []
+    for (let j = 1; j<=10; j++){
+        numbers.push(j);
+    };
+    console.log(numbers);
+};
